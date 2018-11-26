dcafa1b932a5babc50a8d226e35fab89a97e92c4
chengfei <chengfei@iuitech.com>
2018-11-26 18:19:26 +0800

    add log middleware

--- koa/controller/home.js
+++
@@ -1,9 +0,0 @@
-const {knex}=require('../util');
-
-module.exports = {
-  index: async(ctx, next) => {
-    let data= await knex.select('*').from('test')
-    // let data={id:1};
-    ctx.response.body = data
-  }
-}
