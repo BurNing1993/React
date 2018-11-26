8a80f03fdf936dbfd5dd1f96bb007933c58f0a15
BurNing1993 <786678257@qq.com>
2018-11-26 20:05:20 +0800

    up

--- mobx/src/App.js
+++ mobx/src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import Header from './header';
+import DevTools from 'mobx-react-devtools'
+
 
 @inject("store")
 @observer
 class App extends Component {
   render() {
-    const {header}=this.props.store.headerStore;
+    const { header } = this.props.store.headerStore;
+    const { list } = this.props.store;
     return (
-      <div> 
-        {
-          console.log(this.props)
-        }
+      <div>
         <Header />
+        {list.map((item,index) => (<div key={index}>{item}</div>))}
         <span>
           {header}
         </span>
+        <button onClick={this.props.store.addItem(44)}>ADD</button>
+        <DevTools />
       </div>
     );
   }
--- mobx/src/store/index.js
+++ mobx/src/store/index.js
@@ -1,8 +1,16 @@
 import headerStore from '../header/store';
+import { action, observable } from 'mobx';
 
 class Store {
     constructor() {
         this.headerStore = new headerStore()
     }
+
+    @observable list=[1,3];
+
+    @action.bound addItem(num){
+        this.list.push(num);
+    }
+    
 }
 export default  new Store();
