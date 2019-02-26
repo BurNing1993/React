4ae0fbd7d5564eec0e8f239b97871b76c5c032a2
chengfei <chengfei@iuitech.com>
2019-02-26 15:14:30 +0800

    up

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import Button from 'antd/lib/button';
 import Router1 from './pages/router1'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Button type="primary">Button</Button>
         <Router1/>
       </div>
     );
--- router-app/src/pages/router1/index.js
+++ router-app/src/pages/router1/index.js
@@ -1,5 +1,72 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Link,
+  Route
+}from 'react-router-dom'
 
-export default function Router(props){
-    return <div>Router</div>
+const Home=()=>{
+  return <div>Home</div>
 }
+
+const About=()=>{
+  return <div>About</div>
+}
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+
+const Topics=({match})=>(
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props vs State
+        </Link>
+      </li>
+    </ul>
+    <Route path={`${match.path}/:topicId`} component={Topic}/>
+    <Route exact path={match.path} render={()=>(
+       <h3>Please select a topic.</h3>
+    )}/>
+
+  </div>
+)
+
+const Router1=()=>(
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+        <li>
+          <Link to='/topics'>Topics</Link>
+        </li>
+      </ul>
+      <hr/>
+      <Route exact path='/' component={Home}/>
+      <Route path='/about' component={About}/>
+      <Route path='/topics' component={Topics}/>
+    </div>
+  </Router>
+)
+
+export default Router1
