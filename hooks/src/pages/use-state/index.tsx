e4f287b07aae52d50de9f513d8773a988a7e5bb0
chengfei <chengfei@iuitech.com>
2019-09-06 11:45:36 +0800

    up

--- hooks/src/pages/use-state/index.tsx
+++ hooks/src/pages/use-state/index.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button,message } from 'antd';
 
 const UseState: React.FC = () => {
-    return (
-        <div>UseState</div>
-    )
+
+  const [count, setCount] = useState(0);
+
+  const minus: Function = () => {
+    if (count - 1 <= 0) {
+      setCount(0)
+      message.info('Count min is 0');
+    } else {
+      setCount(count - 1)
+    }
+  }
+
+  
+  return (
+    <div>
+      <div className="example">
+        <p>Count = {count}</p>
+        <Button type="primary" onClick={() => { setCount(count + 1) }}>+1</Button>
+        <Button type="primary" onClick={() => { minus() }}>-1</Button>
+      </div>
+      <code>
+
+        &lt;p&gt;Sample text here...&lt;/p&gt; <br />
+        &lt;p&gt;And another line of sample text here...&lt;/p&gt;
+      </code>
+    </div>
+  )
 }
 export default UseState;
